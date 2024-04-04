d4fc58b5e27cf13e02688d9400d1cdc83502d670
martindelosrios <martindelosrios13@gmail.com>
2024-04-04 14:39:55 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# BATMAN pacakage
+## Installation
+
+As this is a dev version for installing BATMAN package you have to download the source doing:
+
+`git clone git@github.com:Martindelosrios/BATMAN.git`
+
+Then, in the BATMAN directory just do
+
+`pip install .`
+
+Soon you will be able to do
+
+`pip install BATMAN`
+
+## Tutorial
+
+In [Examples](https://github.com/Martindelosrios/BATMAN/tree/dev/EXAMPLES) you can find some examples to start playing with BATMAN
---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+with open("README.md", "r") as fp:
+    LONG_DESCRIPTION = fp.read()
+
+REQUIREMENTS = ["numpy", "matplotlib"]
+
+setup(
+    name="BATMAN",
+    version="0.1",
+    description="Implementation of ....",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    author="Martin de los Rios, Andres Perez & David Cerdeño",
+    author_email=" martindelosrios13@gmail.com ",
+    url=" https://github.com/martindelosrios/BATMAN",
+    py_modules=["ez_setup"],  # < - - - - - - - aca van los modulos
+    packages=["BATMAN"],  # < - -- - - - - aca van los paquetes
+    exclude_package_data={"": ["tests"]},
+    include_package_data=True,  # < - - - - - -- solo si hay datos
+    license="The MIT License",
+    install_requires=REQUIREMENTS,
+    keywords=["BATMAN", "Dark matter"],
+    classifiers=[
+        " Development Status :: 4 - Beta",
+        " Intended Audience :: Education",
+        " Intended Audience :: Science/Research",
+        " License :: OSI Approved :: MIT License",
+        " Operating System :: OS Independent",
+        " Programming Language :: Python",
+        " Programming Language :: Python :: 3.8",
+        " Programming Language :: Python :: Implementation :: CPython",
+        " Topic :: Scientific/Engineering",
+    ],
+)
